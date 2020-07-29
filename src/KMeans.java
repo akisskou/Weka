c6760e17b9ec110f9bbd9fa64c72443ec66d6222
akisskou <44747948+akisskou@users.noreply.github.com>
2020-07-29 16:17:09 +0300

    Add files via upload

--- src/KMeans.java
+++ src/KMeans.java
@@ -2,20 +2,35 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.clusterers.SimpleKMeans;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 import weka.clusterers.ClusterEvaluation;
 
+/**
+ * 
+ * @author jason
+ *
+ */
 public class KMeans {
 
 	public static void main(String[] args) {
         // TODO code application logic here
 		try{
-            DataSource src = new DataSource("randomPoints.arff");
+			InputStream input = new FileInputStream("infos.properties");
+			Properties prop = new Properties();
+			// load a properties file
+			prop.load(input);
+			
+            DataSource src = new DataSource(prop.getProperty("trainingFile"));
             Instances dataset = src.getDataSet();
             SimpleKMeans model = new SimpleKMeans();
             
             
             model.setPreserveInstancesOrder(true);
-            model.setNumClusters(2);
+            model.setNumClusters(Integer.valueOf(prop.getProperty("clusters")));
             model.buildClusterer(dataset);
             System.out.println("DataPoints in each cluster:");
     		for (Instance instance : dataset) {
@@ -25,12 +40,11 @@ public class KMeans {
             }
  
             ClusterEvaluation eval = new ClusterEvaluation();
-            DataSource src1 = new DataSource("randomPoints.test.arff");
+            DataSource src1 = new DataSource(prop.getProperty("evaluationFile"));
             Instances tdt = src1.getDataSet();
             eval.setClusterer(model);
             eval.evaluateClusterer(tdt);
             System.out.println(eval.clusterResultsToString());
-            //System.out.println("# of clusters: " + eval.getNumClusters());
 		}
         catch(Exception e)
         {
---
+++ src/Naive_Bayes.java
@@ -0,0 +1,42 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.Evaluation;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+/**
+ * 
+ * @author jason
+ *
+ */
+public class Naive_Bayes {
+
+	public static void main(String[] args) throws Exception {
+		InputStream input = new FileInputStream("infos.properties");
+		Properties prop = new Properties();
+		// load a properties file
+		prop.load(input);
+		
+	    DataSource source = new DataSource(prop.getProperty("trainingFile"));
+	    Instances train = source.getDataSet();
+	    train.setClass(train.attribute(prop.getProperty("attribute")));  // target attribute: (outlook)
+      
+	    //build model
+	    NaiveBayes model=new NaiveBayes();
+	    model.buildClassifier(train);
+	    System.out.println("=== Classifier model (full training set) ===\n");
+	    System.out.println(model);
+
+	    //evaluation
+	    DataSource testSource = new DataSource(prop.getProperty("evaluationFile"));
+	    Instances test = testSource.getDataSet();
+	    test.setClass(test.attribute(prop.getProperty("attribute")));
+	    Evaluation eval = new Evaluation(test);
+	    eval.evaluateModel(model,test);
+	    System.out.println("=== Evaluation on test set ===");
+	    System.out.println(eval.toSummaryString());
+	}	
+}
